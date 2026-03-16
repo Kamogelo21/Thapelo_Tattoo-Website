66a1760b2771762c0cef297d0298510d069dd6d3
Kamogelo Meno <kamenojhb024@student.wethinkcode.co.za>
2026-03-16 17:12:25 +0200

    Update CORS origin

--- src/front-end/script.js
+++ src/front-end/script.js
@@ -1,7 +1,7 @@
 const BACKEND_URL =
   window.location.hostname === "localhost"
     ? "http://localhost:3000"
-    : "https://thaps-ink.onrender.com";
+    : "https://thapsink.onrender.com";
 
 const form = document.getElementById('bookingForm');
 const dateInput = document.getElementById('date');
